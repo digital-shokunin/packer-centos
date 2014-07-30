1dce7015491b7700e6f71c7c39f98dee54251b44
David <david.mitchell@digital-shokunin.net>
2014-07-30 17:15:50 -0400

    Switch to 7

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -1,5 +1,5 @@
 # Make sure udev doesn't block our network
-if grep -q -i "release 6" /etc/redhat-release ; then
+if grep -q -i "release 7" /etc/redhat-release ; then
     rm /etc/udev/rules.d/70-persistent-net.rules
     mkdir /etc/udev/rules.d/70-persistent-net.rules
     rm /lib/udev/rules.d/75-persistent-net-generator.rules
